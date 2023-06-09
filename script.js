7997a414177cc6a2e4283f291e6d78a253aca58c
Nathan Reents <130861854+NJRFirstOfHisName@users.noreply.github.com>
2023-06-09 09:52:27 -0600

    Implement all calculator functions

--- script.js
+++ script.js
@@ -15,25 +15,103 @@ function divide(a,b) {
 }
 
 function runCalculator() {
-    let a = 0, b = 0, operator = null;
+    let memory = {
+        a : "0",
+        b: "",
+        input: "",
+        op: ""
+    }
     const calculator = document.querySelector('#calculator');
     const display = document.querySelector('#display')
-    const allButtons = document.querySelectorAll("btn");
+    const allButtons = document.querySelectorAll("div.btn");
     allButtons.forEach((button) => {
         button.addEventListener('click', () => {
-            input = button.textContent;
-            if(a === 0 && !isNaN(input)) {
-                a += input;
-            } else if (a != 0 && isNaN(input)) {
-                operator = input;
-            } else if (a != 0 && !isNaN(input) && b === 0) {
-                b += input;
-            } else if (a != 0 && isNaN(input) && b != 0) {
-                display.textContent = operate(a, b, input);
+            memory.input = button.textContent;
+            if (button.className === "numBtn btn") {
+                memory = digitPress(memory);
+            } else if (button.className === "opBtn btn") {
+                memory = operatorPress(memory);
+            } else if (button.className === "eqBtn btn") {
+                memory = equalsPress(memory); 
+            } else if (button.className === "clrBtn btn") {
+                memory = clearPress(memory);
+            } else {
+                console.log("Error with button listener")
             }
         })
     })
 }
+
+function digitPress(memory) {
+    if (memory.a === "0"){
+        memory.a = memory.input;
+        display.textContent = memory.a;
+    } else if (memory.op === "") {
+        memory.a += memory.input;
+        display.textContent = memory.a;
+    } else {
+        if (memory.b === "0") {
+            memory.b = memory.input;
+        } else {
+            memory.b += memory.input;
+        }
+        display.textContent = memory.b;
+    }
+    return memory;
+}
+
+function operatorPress(memory) {
+    if (memory.a === "0") {
+        return memory;
+    } else if (memory.b === "0") {
+        memory.op = memory.input;
+    } else {
+        a = parseInt(memory.a);
+        b = parseInt(memory.b);
+        switch(memory.op) {
+            case "+":
+                memory.a = add(a,b);
+                break;
+            case "-":
+                memory.a = subtract(a,b);
+                break;
+            case "*":
+                memory.a = multiply(a,b);
+                break;
+            case "/":
+                memory.a = divide(a,b);
+                break;
+        }
+        display.textContent = memory.a;
+        memory.b = "0";
+        memory.op = memory.input;
+    }
+    return memory;
+}
+
+function equalsPress(memory) {
+    if (memory.a === "0" || memory.op === "" || memory.b === "") {
+        return memory;
+    } else {
+        memory.input = memory.op;
+        memory = operatorPress(memory);
+        memory.op = "";
+        memory.b = "";
+        return memory;
+    }
+}
+
+function clearPress(memory) {
+    memory = {
+        a : "0",
+        b: "",
+        input: "",
+        op: ""
+    }
+    display.textContent = "0";
+    return memory;
+}
+
 
 function operate(a, b, op) {
     switch(op) {
@@ -52,3 +130,4 @@ function operate(a, b, op) {
     }
 }
 
+runCalculator();
